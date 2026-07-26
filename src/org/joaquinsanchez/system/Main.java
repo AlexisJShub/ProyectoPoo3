479940dd9570580c4e0e27051401e5851c475003
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 22:36:37 -0600

    013 refactor(main) Se agrego el metodo para que arranque junto con el archivo fxml

--- src/org/joaquinsanchez/system/Main.java
+++ src/org/joaquinsanchez/system/Main.java
@@ -1,12 +1,11 @@
 package org.joaquinsanchez.system;
 
+
 import javafx.application.Application;
-import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TabPane;
 import javafx.stage.Stage;
-import org.joaquinsanchez.controller.PaqueteController;
-import org.joaquinsanchez.view.PaqueteView;
 
 public class Main extends Application {
 
@@ -16,14 +15,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage escenarioPrincipal) throws Exception {
-        PaqueteView vista = new PaqueteView();
-        TabPane raiz = vista.getPanelPestana();
-        PaqueteController controlador = new PaqueteController(vista);
+       
+            Parent raiz = FXMLLoader.load(getClass().getResource("/org/joaquinsanchez/view/PaqueteView.fxml"));
 
-        Scene escena = new Scene(raiz, 480, 640);
-        escenarioPrincipal.setTitle("Sistema de Gestión de Paquetes");
-        escenarioPrincipal.setScene(escena);
-        escenarioPrincipal.show();
+            Scene escena = new Scene(raiz, 480, 640);
+            escenarioPrincipal.setTitle("Sistema de Gestión de Paquetes");
+            escenarioPrincipal.setScene(escena);
+            escenarioPrincipal.show();
+        }
     }
-
-}
