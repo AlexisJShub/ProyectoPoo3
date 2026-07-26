74485eb0d4d2a15be0f977605d789b8745a9813c
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 22:21:48 -0600

    009 refactor(model) Se agregaron a Sobre, calculo de tarifa fija por peso

--- src/org/joaquinsanchez/model/Sobre.java
+++ src/org/joaquinsanchez/model/Sobre.java
@@ -1,12 +1,43 @@
 package org.joaquinsanchez.model;
 
 
-public class Sobre extends Paquete{
+public class Sobre extends Paquete {
+
+    //tarifa fija de envio por kilogramo, en quetzales
+    private static final float TARIFA_POR_KG = 5.0f;
+
+    private float peso; //peso en kg
+
+    public Sobre() {
+    }
+
+    public Sobre(String id, float peso) {
+        super(id);
+        this.peso = peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
 
     @Override
     public double calculoTotal() {
-        System.out.println("");
-        return 0;
+        //costo del sobre segun su peso
+        return (double) this.peso * TARIFA_POR_KG;
     }
-    
+
+    @Override
+    public String getTipo() {
+        return "Sobre";
+    }
+
+    @Override
+    public String getDetalle() {
+        return String.format("Peso: %.2f kg", peso);
+    }
+
 }
