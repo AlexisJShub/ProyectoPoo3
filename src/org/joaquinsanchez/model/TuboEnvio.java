68f9ada436baa5e065eccf8badbf6f0298b1f183
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 22:19:11 -0600

    008 feat(model) Se creo la clase TuboEnvio con su calculo volumen y sus detalles

--- src/org/joaquinsanchez/model/TuboEnvio.java
+++ src/org/joaquinsanchez/model/TuboEnvio.java
@@ -2,10 +2,49 @@ package org.joaquinsanchez.model;
 
 public class TuboEnvio extends Paquete {
 
+    private float longitud; 
+    private float diametro; 
+
+    public TuboEnvio() {
+    }
+
+    public TuboEnvio(String id, float longitud, float diametro) {
+        super(id);
+        this.longitud = longitud;
+        this.diametro = diametro;
+    }
+
+    public void setLongitud(float longitud) {
+        this.longitud = longitud;
+    }
+
+    public float getLongitud() {
+        return this.longitud;
+    }
+
+    public void setDiametro(float diametro) {
+        this.diametro = diametro;
+    }
+
+    public float getDiametro() {
+        return this.diametro;
+    }
+
     @Override
     public double calculoTotal() {
-        System.out.println("");
-        return 0;
+        //volumen del cilindro (pi * r^2 * altura), igual criterio volumetrico que CajaEstandar
+        double radio = this.diametro / 2.0;
+        return Math.PI * Math.pow(radio, 2) * this.longitud;
     }
-    
+
+    @Override
+    public String getTipo() {
+        return "Tubo de Envío";
+    }
+
+    @Override
+    public String getDetalle() {
+        return String.format("Long: %.1f cm, Diám: %.1f cm", longitud, diametro);
+    }
+
 }
