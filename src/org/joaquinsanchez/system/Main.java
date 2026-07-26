bfcae9ceeb7e7ebf327e6bf0245cf05a1e20e673
Joaquin Sanchez <jsanchez-2026117@kinal.edu.gt>
2026-07-25 21:47:17 -0600

    007 feat(main) Se agrego el arranque de todo el stage y scene

--- src/org/joaquinsanchez/system/Main.java
+++ src/org/joaquinsanchez/system/Main.java
@@ -1,17 +1,14 @@
 package org.joaquinsanchez.system;
 
 import javafx.application.Application;
+import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TabPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import org.joaquinsanchez.model.Paquete;
+import org.joaquinsanchez.controller.PaqueteController;
 import org.joaquinsanchez.view.PaqueteView;
 
 public class Main extends Application {
-
-
-
 
     public static void main(String[] args) {
         launch(args);
@@ -19,13 +16,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage escenarioPrincipal) throws Exception {
-        PaqueteView vista = new PaqueteView(); 
+        PaqueteView vista = new PaqueteView();
         TabPane raiz = vista.getPanelPestana();
-        Scene escena = new Scene(raiz, 450, 600);
+        PaqueteController controlador = new PaqueteController(vista);
+
+        Scene escena = new Scene(raiz, 480, 640);
+        escenarioPrincipal.setTitle("Sistema de Gestión de Paquetes");
         escenarioPrincipal.setScene(escena);
-        escenarioPrincipal.show(); 
-        
-       
+        escenarioPrincipal.show();
     }
 
 }
